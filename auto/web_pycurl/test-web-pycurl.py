3fbfcb63fa9197de73f2f3fa157f58c3e3825010
zhangyanan@daojia.com.cn <zhangyanan@daojia.com.cn>
2016-11-23 15:28:34 +0800

    添加注释

--- auto/web_pycurl/test-web-pycurl.py
+++ auto/web_pycurl/test-web-pycurl.py
@@ -6,37 +6,37 @@ import time
 import sys
 import pycurl
 
-URL="http://www.baidu.com"
-c = pycurl.Curl()
-c.setopt(pycurl.URL, URL)
-c.setopt(pycurl.CONNECTTIMEOUT, 5)
-c.setopt(pycurl.TIMEOUT, 5)
-c.setopt(pycurl.NOPROGRESS, 1)
-C.setopt(pycurl.FORBID_REUSE, 1)
-c.setopt(pycurl.MAXREDIRS, 1)
-c.setopt(pycurl.DNS_CACHE_TIMEOUT, 30)
-
+URL="http://www.baidu.com" #目标url
+c = pycurl.Curl()          #创建一个curl对象
+c.setopt(pycurl.URL, URL)  #定义请求的url常量
+c.setopt(pycurl.CONNECTTIMEOUT, 5) #定义请求连接的等待时间
+c.setopt(pycurl.TIMEOUT, 5) #定义请求超时时间
+c.setopt(pycurl.NOPROGRESS, 1) #屏蔽下载进度条
+c.setopt(pycurl.FORBID_REUSE, 1) #完成交互后强制断开连接，不重用
+c.setopt(pycurl.MAXREDIRS, 1) #指定http重定向的最大数为1
+c.setopt(pycurl.DNS_CACHE_TIMEOUT, 30) 设置保存DNS信息的时间为30秒
+#创建一个文件对象，以“wb”方式打开，用来存储返回的http头部以及页面内容
 indexfile = open(os.path.dirname(os.path.realpath(__file__))+"/content.txt","wb")
-c.setopt(pycurl.WRITEHEADER, indexfile)
-c.setopt(pycurl.WRITEDATA, indexfile)
+c.setopt(pycurl.WRITEHEADER, indexfile) #将返回的http header定向到indexfile文件对象
+c.setopt(pycurl.WRITEDATA, indexfile) #将返回的HTML定向到indexfile文件对象
 
 try:
-	c.perform()
+	c.perform() #提交请求
 except Exception,e:
 	print "connecion error:"+str(e)
 	indexfile.close()
 	c.close()
 	sys.exit()
 	
-NAMELOOKUP_TIME = c.getinfo(c.NAMELOOKUP_TIME)
-CONNECT_TIME = c.getinfo(c.CONNECT_TIME)
-PRETRANSFER_TIME = c.getinfo(c.PRETRANSFER_TIME)
-STARTTRANSFER_TIME = c.getinfo(c.STARTTRANSFER_TIME)
-TOTAL_TIME = c.getinfo(c.TOTAL_TIME)
-HTTP_CODE = c.getinfo(c.HTTP_CODE)
-SIZE_DOWNLOAD = c.getinfo(c.SIZE_DOWNLOAD)
-HEADER_SIZE = c.getinfo(c.HEADER_SIZE)
-SPEED_DOWNLOAD = c.getinfo(c.SPEED_DOWNLOAD)
+NAMELOOKUP_TIME = c.getinfo(c.NAMELOOKUP_TIME) #获取dns解析时间
+CONNECT_TIME = c.getinfo(c.CONNECT_TIME) #获取建立了解时间
+PRETRANSFER_TIME = c.getinfo(c.PRETRANSFER_TIME)#获取建立连接到准备传输消耗的时间
+STARTTRANSFER_TIME = c.getinfo(c.STARTTRANSFER_TIME) #获取建立连接到传输开始消耗的时间
+TOTAL_TIME = c.getinfo(c.TOTAL_TIME) #获取传输的总时间
+HTTP_CODE = c.getinfo(c.HTTP_CODE) #获取http状态码
+SIZE_DOWNLOAD = c.getinfo(c.SIZE_DOWNLOAD) #获取下载数据包的大小
+HEADER_SIZE = c.getinfo(c.HEADER_SIZE)#获取http头部大小
+SPEED_DOWNLOAD = c.getinfo(c.SPEED_DOWNLOAD)#获取平均下载速度
 
 print "HTTP:状态码：%s" % (HTTP_CODE)
 print "DNS解析时间：%.2f ms" %(NAMELOOKUP_TIME*1000)
