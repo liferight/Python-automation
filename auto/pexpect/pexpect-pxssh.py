e79439ff32281833f8f0925e516a7a7854d00d9f
ZYN <zhangyanan@daojia.com.cn>
2016-11-24 19:18:15 +0800

    pexpect下pxssh类使用

---
+++ auto/pexpect/pexpect-pxssh.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding:utf-8
+#create zyn 2016
+import pxssh
+import getpass
+
+try:
+	s = pxssh.pxssh()
+	hostname = raw_input('hostname:')
+	username = raw_input('username:')
+	password = getpass.getpass('please input password:')
+	s.login (hostname, username, password)
+	s.sendline ('uptime')
+	s.prompt()
+	print s.before
+	
+	s.sendline('ls -l')
+	s.prompt()
+	print s.before
+	s.sendline('df')
+	s.prompt()
+	print s.before
+	s.logout()
+except pxssh.ExceptionPxssh, e:
+	print "pxssh failed on login."
+	print str(e)
